ea4070c933da42421987dc6f98da9853a4c7d909
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2021-03-28 16:55:48 +0200

    add class

---
+++ Command.java
@@ -0,0 +1,49 @@
+ 
+
+public class Command
+{
+    private String aCommandWord;
+    private String aSecondWord;
+    
+    /**
+     * Command's class Constructor 
+     */
+    public Command(final String pCommandWord,final String pSecondWord)
+    {
+        this.aCommandWord = pCommandWord;
+        this.aSecondWord = pSecondWord;
+    } //Command | Constructor
+    
+    /**
+     * getter for the private field aCommandWord
+     */
+    public String getCommandWord() 
+    {
+        return this.aCommandWord;
+    } //getCommandWord
+    /**
+     * getter for the private field aSecondWord
+     */
+    public String getSecondWord()
+    {
+        return this.aSecondWord;
+    } //getSecondWord
+    /**
+     * Check if the command have a second word
+     * @return true if command have one else  false
+     */
+    public boolean hasSecondWord()
+    {
+        return this.aSecondWord != null;
+    } //hasSecondWord()
+    /**
+     * check if the command is Unknown 
+     * return true if the first word is null else return false
+     */
+    public boolean isUnknown()
+    {
+        return this.aCommandWord == null;
+    } //isUknown()
+    
+    
+} // Command
